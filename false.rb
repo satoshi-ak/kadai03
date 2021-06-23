283734520e9ebd25953f51d08c4251f3ba57fb57
satoshi aoki <satozi.ak.3250@gmail.com>
2021-06-24 00:42:11 +0900

    commit

--- false.rb
+++ false.rb
@@ -4,7 +4,7 @@ cgi = CGI.new
 # データを受け取った後、HTMLの形式でレスポンスを返す
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
   # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
-  get = cgi['false']
+  get = cgi['quality']
   # HTMLでレスポンスを返却する
   "<html>
     <body>
--- test.rb
+++ test.rb
@@ -1,13 +1,13 @@
 require 'webrick'
 server = WEBrick::HTTPServer.new({
-  :DocumentRoot => '.',
+  :DocumentRoot => '/',
   :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
   :Port => '3000',
 })
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/exam', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.mount('/give.cgi', WEBrick::HTTPServlet::CGIHandler, 'give.rb')
